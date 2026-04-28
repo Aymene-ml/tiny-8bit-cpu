b9f60107a5a27babdf158598338abb98945ba408
Aymene-ml <korichiaymen27@gmail.com>
2026-04-28 23:43:20 +0100

    Add carry output to ALU testbench

--- test/unit/tb_alu.v
+++ test/unit/tb_alu.v
@@ -6,11 +6,13 @@ module tb_alu;
   reg [7:0] acc_i;
   reg [7:0] mem_i;
   wire [7:0] result_o;
+  wire carry_o;
 
   alu u_dut (
       .op_i(op_i),
       .acc_i(acc_i),
       .mem_i(mem_i),
-      .result_o(result_o)
+      .result_o(result_o),
+      .carry_o(carry_o)
   );
 endmodule
